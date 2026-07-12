bd65bdf7d7cbc58521f5609b961fd786964166cf
leslemon1314-beep <leslemon1314@gmail.com>
2026-07-12 22:01:12 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyApQQ9i5BeoRa-HxjjQRi91SG-Wg04wcHo",
+  authDomain: "lana-page.firebaseapp.com",
+  databaseURL: "https://lana-page-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "lana-page",
+  storageBucket: "lana-page.firebasestorage.app",
+  messagingSenderId: "818320050436",
+  appId: "1:818320050436:web:dc3241ee43234f3bb4e851"
+};
